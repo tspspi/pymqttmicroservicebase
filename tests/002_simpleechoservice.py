9d7587fccec511d47b1c33c4e12d9bffb095ccd9
tspspi <11219971+tspspi@users.noreply.github.com>
2022-07-27 12:46:19 +0200

    Showing how to parse topics ...

--- tests/002_simpleechoservice.py
+++ tests/002_simpleechoservice.py
@@ -7,17 +7,30 @@ class EchoService(MQTTBaseService):
 			applicationName = "MQTTEchoService",
 
 			topicHandlers = [
-				{ 'topic' : 'echoservice/echo',    'handler' : [ self._handleEchoRequestMessage ] },
-				{ 'topic' : 'echoservice/version', 'handler' : [ self._handleEchoVersionRequest ] },
-				{ 'topic' : 'echoservice/echon/#', 'handler' : [ self._handleEchoVersionRequest ] }
+				{ 'topic' : 'echoservice/echo',            'handler' : [ self._handleEchoRequestMessage  ] },
+				{ 'topic' : 'echoservice/version/request', 'handler' : [ self._handleEchoVersionRequest  ] },
+				{ 'topic' : 'echoservice/echo/#',          'handler' : [ self._handleEchoRequestMessageN ] }
 			]
 		)
 
 	def _handleEchoRequestMessage(self, topic, msg):
 		print(f"Received echo request with payload {msg.payload}")
+		self.mqttPublish('echoservice/reply', { 'reply' : True, 'payload' : msg.payload })
 
 	def _handleEchoVersionRequest(self, topic, msg):
 		print("Received version query request")
+		self.mqttPublish('echoservice/version', { 'version' : '0.0.1' })
+
+	def _handleEchoRequestMessageN(self, topic, msg):
+		topic = topic.split("/")
+		try:
+			N = int(topic[2])
+			print(f"Received echo request for {N} responses")
+
+			for i in range(N):
+				self.mqttPublish('echoservice/reply', { 'reply' : True, 'payload' : msg.payload, 'n' : i })
+		except:
+			pass
 
 	def __enter__(self):
 		return self
